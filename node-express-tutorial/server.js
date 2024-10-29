b7b93f179b9dfe7f3bfe2c361ded161d4deed34e
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-10-29 17:43:06 +0300

    making first get and delete requests

--- node-express-tutorial/server.js
+++ node-express-tutorial/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 4999;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    console.log('GET request received');
+});
+
+app.delete('/', (req, res) => {
+    console.log('DELETE request received');
 });
